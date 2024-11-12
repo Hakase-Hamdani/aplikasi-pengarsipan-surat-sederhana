02972c2d09e87886c6ce1d8f9c33aae179fc6eed
Hakase-Hamdani <mhdhamdani.formal@gmail.com>
2024-11-12 15:49:27 +0800

    dokumentasi logic index.php

--- index.php
+++ index.php
@@ -1,29 +1,21 @@
 <?php
+    //jangan di macam-macami
     ob_start();
     session_start();
 
-    //cek session
+    //cek session. Jika session admin, lompat ke admin.php
     if(isset($_SESSION['admin'])){
         header("Location: ./admin.php");
         die();
     }
-
+    
+    //jangan di macam-macami
     require_once 'include/config.php';
     require_once 'include/functions.php';
+    //jangan ubah any instances of $config, variabel buat koneksi database
     $config = conn($host, $username, $password, $database);
 ?>
-<!--
 
-Name        : Aplikasi Sederhana Manajemen Surat Menyurat
-Version     : v1.0.1
-Description : Aplikasi untuk mencatat data surat masuk dan keluar secara digital.
-Date        : 2016
-Developer   : M. Rudianto
-Phone/WA    : 0852-3290-4156
-Email       : rudi@masrud.com
-Website     : https://masrud.com
-
--->
 <!doctype html>
 <html lang="en">
 
@@ -36,11 +28,6 @@ Website     : https://masrud.com
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-    <?php
-        $query = mysqli_query($config, "SELECT logo from tbl_instansi");
-        list($logo) = mysqli_fetch_array($query);
-        echo '<link rel="shortcut icon" href="upload/'.$logo.'">';
-    ?>
     <!-- Meta END -->
 
     <!--[if lt IE 9]>
@@ -187,38 +174,32 @@ Website     : https://masrud.com
                     <!-- Row Form START -->
                     <div class="row">
 
-                    <?php
-                        $query = mysqli_query($config, "SELECT * FROM tbl_instansi");
-                        while($data = mysqli_fetch_array($query)){
-                    ?>
                     <!-- Logo and title START -->
+                    <!-- Buat Logo dan Nama Aplikasi di login -->
                     <div class="col s12">
                         <div class="card-content">
                             <h5 class="center" id="title">Aplikasi Manajemen Surat</h5>
-                            <?php echo '<img id="logo" src="upload/'.$data['logo'].'">';?>
+                            <img id="logo" src="upload/logo.png">'
                             <h4 class="center" id="smk">
-                            <?php echo ''.$data['nama'].'';?>
                             </h4>
                             <div class="batas"></div>
                         </div>
                     </div>
-                    <!-- Logo and title END -->
+                    <!-- Logic login -->
                     <?php
-                        }
-                    ?>
-
-                    <?php
-                        if(isset($_REQUEST['submit'])){
+                        if(isset($_REQUEST['submit'])){ //even submit, terjadi saat tombol LOGIN ditekan
 
                             //validasi form kosong
-                            if($_REQUEST['username'] == "" || $_REQUEST['password'] == ""){
+                            if($_REQUEST['username'] == "" || $_REQUEST['password'] == ""){ //cek apakah kolom username dan password terisi saat event submit
                                 echo '<div class="upss red-text"><i class="material-icons">error_outline</i> <strong>ERROR!</strong> Username dan Password wajib diisi.
                                 <a class="btn-large waves-effect waves-light blue-grey col s11" href="" style="margin: 20px 0 0 5px;"><i class="material-icons md-24">arrow_back</i> Kembali ke login form</a></div>';
                             } else {
-
+                                
+                                //ambil dari kolom username dan password
                                 $username = trim(htmlspecialchars(mysqli_real_escape_string($config, $_REQUEST['username'])));
                                 $password = trim(htmlspecialchars(mysqli_real_escape_string($config, $_REQUEST['password'])));
 
+                                //cek dari tbl_user, apakah username dan password di atas ada.
                                 $query = mysqli_query($config, "SELECT id_user, username, nama, nip, admin FROM tbl_user WHERE username=BINARY'$username' AND password=MD5('$password')");
 
                                 if(mysqli_num_rows($query) > 0){
@@ -233,7 +214,7 @@ Website     : https://masrud.com
 
                                     header("Location: ./admin.php");
                                     die();
-                                } else {
+                                } else { //Jika jika user atau password tidak ditemukan
 
                                     //session error
                                     $_SESSION['errLog'] = '<center>Username & Password tidak ditemukan!</center>';
@@ -248,13 +229,13 @@ Website     : https://masrud.com
                     <form class="col s12 m12 offset-4 offset-4" method="POST" action="" >
                         <div class="row">
                             <?php
-                                if(isset($_SESSION['errLog'])){
+                                if(isset($_SESSION['errLog'])){ //Error jika user atau password tidak ditemukan
                                     $errLog = $_SESSION['errLog'];
                                     echo '<div id="alert-message" class="error red lighten-5"><div class="center"><i class="material-icons">error_outline</i> <strong>LOGIN GAGAL!</strong></div>
                                     '.$errLog.'</div>';
                                     unset($_SESSION['errLog']);
                                 }
-                                if(isset($_SESSION['err'])){
+                                if(isset($_SESSION['err'])){ //WHAT THE HELL IS THIS??? //Kayaknya buat "fatal error" bullshit
                                     $err = $_SESSION['err'];
                                     echo '<div id="alert-message" class="error red lighten-5"><div class="center"><i class="material-icons">error_outline</i> <strong>ERROR!</strong></div>
                                     '.$err.'</div>';
