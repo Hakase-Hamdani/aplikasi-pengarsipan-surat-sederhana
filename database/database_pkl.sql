5c4131f987fe28ea6c49a456ba6a308836f64ec1
Hakase-Hamdani <mhdhamdani.formal@gmail.com>
2024-11-19 10:32:51 +0800

    added default database

---
+++ database/database_pkl.sql
@@ -0,0 +1,216 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 19, 2024 at 03:30 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database_pkl`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_instansi`
+--
+
+CREATE TABLE `tbl_instansi` (
+  `id_instansi` int(10) NOT NULL,
+  `institusi` varchar(50) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `alamat` varchar(250) DEFAULT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  `logo` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_klasifikasi`
+--
+
+CREATE TABLE `tbl_klasifikasi` (
+  `id_klasifikasi` int(10) NOT NULL,
+  `id_instansi` int(10) DEFAULT NULL,
+  `kode` varchar(50) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `uraian` mediumtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_staf`
+--
+
+CREATE TABLE `tbl_staf` (
+  `id_staff` int(10) NOT NULL,
+  `id_user` int(10) DEFAULT NULL,
+  `id_instansi` int(10) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `NIP` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbl_staf`
+--
+
+INSERT INTO `tbl_staf` (`id_staff`, `id_user`, `id_instansi`, `nama`, `NIP`) VALUES
+(1, 1, NULL, 'Muhammad Hamdani', '2110010302');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_surat_keluar`
+--
+
+CREATE TABLE `tbl_surat_keluar` (
+  `id_surat` int(10) NOT NULL,
+  `id_staf` int(10) DEFAULT NULL,
+  `id_klasifikasi` int(10) DEFAULT NULL,
+  `no_agenda` int(10) DEFAULT NULL,
+  `tujuan` varchar(250) DEFAULT NULL,
+  `no_surat` varchar(250) DEFAULT NULL,
+  `isi` mediumtext DEFAULT NULL,
+  `kode` varchar(30) DEFAULT NULL,
+  `tgl_surat` date DEFAULT NULL,
+  `tgl_catat` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `file` varchar(250) DEFAULT NULL,
+  `keterangan` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(10) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `role` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `username`, `password`, `role`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1),
+(2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_instansi`
+--
+ALTER TABLE `tbl_instansi`
+  ADD PRIMARY KEY (`id_instansi`);
+
+--
+-- Indexes for table `tbl_klasifikasi`
+--
+ALTER TABLE `tbl_klasifikasi`
+  ADD PRIMARY KEY (`id_klasifikasi`),
+  ADD KEY `id_instansi` (`id_instansi`);
+
+--
+-- Indexes for table `tbl_staf`
+--
+ALTER TABLE `tbl_staf`
+  ADD PRIMARY KEY (`id_staff`),
+  ADD KEY `id_instansi` (`id_instansi`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `tbl_surat_keluar`
+--
+ALTER TABLE `tbl_surat_keluar`
+  ADD PRIMARY KEY (`id_surat`),
+  ADD KEY `id_klasifikasi` (`id_klasifikasi`),
+  ADD KEY `id_staf` (`id_staf`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_instansi`
+--
+ALTER TABLE `tbl_instansi`
+  MODIFY `id_instansi` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_klasifikasi`
+--
+ALTER TABLE `tbl_klasifikasi`
+  MODIFY `id_klasifikasi` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_staf`
+--
+ALTER TABLE `tbl_staf`
+  MODIFY `id_staff` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_surat_keluar`
+--
+ALTER TABLE `tbl_surat_keluar`
+  MODIFY `id_surat` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_klasifikasi`
+--
+ALTER TABLE `tbl_klasifikasi`
+  ADD CONSTRAINT `tbl_klasifikasi_ibfk_1` FOREIGN KEY (`id_instansi`) REFERENCES `tbl_instansi` (`id_instansi`);
+
+--
+-- Constraints for table `tbl_staf`
+--
+ALTER TABLE `tbl_staf`
+  ADD CONSTRAINT `tbl_staf_ibfk_1` FOREIGN KEY (`id_instansi`) REFERENCES `tbl_instansi` (`id_instansi`),
+  ADD CONSTRAINT `tbl_staf_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tbl_user` (`id_user`);
+
+--
+-- Constraints for table `tbl_surat_keluar`
+--
+ALTER TABLE `tbl_surat_keluar`
+  ADD CONSTRAINT `tbl_surat_keluar_ibfk_1` FOREIGN KEY (`id_klasifikasi`) REFERENCES `tbl_klasifikasi` (`id_klasifikasi`),
+  ADD CONSTRAINT `tbl_surat_keluar_ibfk_2` FOREIGN KEY (`id_staf`) REFERENCES `tbl_staf` (`id_staff`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
